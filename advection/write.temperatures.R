ddfcce883bf1e24d073e877af5b7eb5f27acdc3a
PatchDynamics <xone@fromthegut.org>
2016-10-28 14:07:54 -0700

    Meteorology and water temp

--- advection/write.temperatures.R
+++ advection/write.temperatures.R
@@ -3,10 +3,11 @@ names(temperatures) = c('Date', 'Mile', 'Temp', 'StdDev')
 temperatures$Year = floor(temperatures$Date)
 for( year in 2005:2015){
   data = filter(temperatures, Year==year)
-  filename = paste('TIN',year,'.npt')
+  filename = paste0('TIN',year,'.npt')
   write(paste0('****************** Temperature File ****************'),
         file = filename, append=FALSE)
   write('', file=filename, append=TRUE)
+  write('    JDAY     TIN', file=filename, append=TRUE)
   formatted.df = sprintf("%8.0f%8.0f", index(data), data$Temp)
   write(formatted.df, file=filename, append=TRUE)
 }
