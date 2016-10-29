79c41bf7cf77d75ead0620ca1aa07627bf15070a
PatchDynamics <xone@fromthegut.org>
2016-10-28 17:25:15 -0700

    Tweaks to temperature files

--- advection/write.temperatures.R
+++ advection/write.temperatures.R
@@ -8,6 +8,32 @@ for( year in 2005:2015){
         file = filename, append=FALSE)
   write('', file=filename, append=TRUE)
   write('    JDAY     TIN', file=filename, append=TRUE)
-  formatted.df = sprintf("%8.0f%8.0f", index(data), data$Temp)
+  formatted.df = sprintf("%8.0f%8.2f", index(data), data$Temp)
   write(formatted.df, file=filename, append=TRUE)
 }
+
+
+
+# justcheatinghere
+crs.narr = '+proj=lcc +x_0=5632642.22547 +y_0=4612545.65137 +lat_0=50 +lon_0=-107 +lat_1=50 +lat_2=50 +ellps=WGS84'
+station.locations = shapefile('../../Columbia/GIS/WeatherStationsUsed.shp')
+station.locations = spTransform(station.locations, crs.narr)
+crs(station.locations)
+values = NULL
+first.year=2005
+last.year=2015
+for(year in first.year:last.year){
+  download.file(str_replace(url.dswrf, 'YYYY', year), 'dswrf.nc')
+  uswrf.sfc = brick('uswrf.nc')
+  extracted.values = raster::extract(uswrf.sfc, station.locations)  
+  if(is.null(values)){
+    values = extracted.values
+  } else {
+    values = cbind(values, extracted.values)
+  }
+}
+# NARR: W/m^2  (per day)
+# CE-QUAL-W2: W/m^2
+
+swr.2005.2015 = values
+save(swr.2005.2015, file='swr.2005.2015.Rdata')
