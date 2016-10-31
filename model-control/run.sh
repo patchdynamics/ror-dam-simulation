fb227023c79404df5ac647f4a61cb358722b1976
PatchDynamics <xone@fromthegut.org>
2016-10-30 22:29:21 -0700

    Working on spinups for chaining

--- model-control/run.sh
+++ model-control/run.sh
@@ -1,9 +1,15 @@
+#!/bin/bash
+set -e
+
 # year and day are specified somewhere
 YEAR=2015
 DAY=100
 
 run.model()
 {
+	cp outputs/$1/$YEAR/$DAY/rso$DAY.opt rsi.npt
+	#cp outputs/$1/$YEAR/$DAY/spr.opt spr.opt # looks like it wants to keep adding to this
+	cp inputs/spinup/$1/*.opt .  # testing spinup, appears to want ALL the opt files 
 	wine ../../CE-QUAL-W2-v372/executables/w2\ model/w2_ivf32_v372.exe 
 	mkdir -p outputs/$1/$YEAR/$DAY
 	cp *.opt outputs/$1/$YEAR/$DAY
