d720590771ffb54fb9ae76facd6ee90f0f82086a
PatchDynamics <xone@fromthegut.org>
2016-10-31 17:09:36 -0700

    Pulled QIN as a sum of Anaton, Clearwater, and NF Clearwater discharges

---
+++ advection/write.qin.R
@@ -0,0 +1,18 @@
+library(chron)
+advection = read.csv('Advection.2005.2015.csv')
+julian = advection$X + julian(as.Date('2005-01-01')) - 1
+out = month.day.year(julian)
+qin = advection$Clearwater + advection$Snake + advection$NFClearwater
+for (i in 1:11) {
+  year = (2005:2015)[i]
+  qin.year.q = qin[out$year == year]
+  qin.julian = julian[out$year == year] - julian(as.Date(paste0(year,'-01-01'))) + 1
+  data = data.frame(Julian=qin.julian, Q = qin.year.q)
+  filename = paste0('QIN',year,'.npt')
+  write(paste0('****************** Q In File ****************'),
+        file = filename, append=FALSE)
+  write('', file=filename, append=TRUE)
+  write('    JDAY    QIN', file=filename, append=TRUE)
+  formatted.df = sprintf("%8.0f%8.2f", data$Julian, data$Q)
+  write(formatted.df, file=filename, append=TRUE)
+}
