fc49d9a8d808293bd46a28ab2ad3679741c1e96a
Isabel Bush <ibush@stanford.edu>
2016-11-04 22:55:23 -0700

    Linear Q-learning

--- model-control-chain/runSimulation.py
+++ model-control-chain/runSimulation.py
@@ -2,6 +2,9 @@ import numpy as np
 import subprocess
 import os
 from shutil import copyfile
+from sklearn.utils.extmath import cartesian
+import random
+import re
 
 PROJECT_DIR = "../" #ror-dam-simulation directory
 CE_QUAL_W2_EXE = "../../bin/w2_ivf32_v372.exe"
@@ -11,32 +14,51 @@ CON_FILE = "w2_con.npt"
 TEMPERATURE_FILE = "spr.opt"
 QWO_FILE = "qwo_34.opt"
 QOUT_FILE = "qot_br1.npt"
-RSI_FILE = "rso60.opt"
 CHAINING_FILE = CONTROL_DIR + "scripts/propagate.flow.sh"
+ELEVATION_FILE = "wl.opt"
 
-#TODO: Remove these parameters from the control file
+# Hyperparameters
+EPSILON_GREEDY = 0.2 # TODO: Should start high & decrease over time
+FUTURE_DISCOUNT = 0.95
+STEP_SIZE = 0.01
+
+# Actions
+SPILLWAY_OUTFLOWS = [0, 500]
+POWERHOUSE_OUTFLOWS = [0, 600, 1200]
+HYPOLIMNAL_OUTFLOWS = [0, 500]
+
+# Reward parameters
+MIN_ELEVATION = 200
+MAX_ELEVATION = 225
+
 def modifyControlFile(fileDir, timeStart, timeEnd, year):
     with open(fileDir + CON_FILE, "w") as fout:
         with open(fileDir + "inputs/control/" + TOKENIZED_CON_FILE, "r") as fin:
             for line in fin:
-                line = line.replace("%RSIFN%", RSI_FILE)
-                line = line.replace("%QINFN%", "qin.npt")
-                line = line.replace("%TINFN%", "tin.npt")
-                line = line.replace("%METFN%", "met.npt")
                 line = line.replace("%TMSTRT%", str(timeStart).rjust(8))
                 line = line.replace("%TMEND_%", str(timeStart + timeStep).rjust(8))
                 line = line.replace("%YEAR__%", str(year).rjust(8))
                 fout.write(line)
 
-def setAction(fileDir, timeStart, gatesOn, wb):
+def setAction(fileDir, timeStart, action, wb):
     line = str(timeStart+1).rjust(8)
-    line += str( 1000 if gatesOn[wb][0] else 0 ).rjust(8) #TODO: How much output for "on"
-    line += str( 1000 if gatesOn[wb][1] else 0 ).rjust(8)
+    line += str(action[0]).rjust(8)
+    line += str(action[1]).rjust(8)
+    line += str(action[2]).rjust(8)
     line += "\n"
     with open(fileDir + QOUT_FILE, "a") as f:
         f.write(line)
 
-def getReward(fileDir, gatesOn):
+def getReward(numDams):
+    for i in range(1, numDams+1):
+        wlFile = CONTROL_DIR + "wb" + str(i) + "/" + ELEVATION_FILE
+        elevations = np.genfromtxt(wlFile, delimiter=",")
+        elevation = np.mean(elevations[-1,1:-1])
+        if elevation < MIN_ELEVATION or elevation > MAX_ELEVATION:
+            return -1
+    return 0
+
+'''
     temps = np.genfromtxt(fileDir + TEMPERATURE_FILE, delimiter=",", skip_header=1, usecols = 4)
     # TODO: This is for one dam, do the same for other dams
     if gatesOn[0,1]:
@@ -48,6 +70,7 @@ def getReward(fileDir, gatesOn):
     print temps
     print qPowerGate
     return qPowerGate - np.mean(temps) #TODO: Calculate a reward
+'''
 
 def copyInYearFiles(year, numDams):
     copyfile( CONTROL_DIR + "wb1/inputs/QIN" + str(year) +".npt", CONTROL_DIR + "wb1/qin.npt")
@@ -60,24 +83,64 @@ def copyInYearFiles(year, numDams):
             if os.path.isfile(filename):
                 copyfile( filename , CONTROL_DIR + "wb" + str(wb) + "/" + f)
 
+def calculatePossibleActions():
+    return cartesian((SPILLWAY_OUTFLOWS, POWERHOUSE_OUTFLOWS, HYPOLIMNAL_OUTFLOWS))
+
+# TODO: Real state function here
+def getState(timeStart, year):
+    return np.random.randint(2, size=25)
+
+def getAction(state, weights, possibleActions):
+    if random.random() < EPSILON_GREEDY:
+        return random.randrange(possibleActions.shape[0])
+    else:
+        [bestActionInd, Vopt] = getBestAction(state, weights, possibleActions)
+        return bestActionInd
+
+def getBestAction(state, weights, possibleActions):
+    Qopts = np.empty(possibleActions.shape[0])
+    for actionInd in range(possibleActions.shape[0]):
+        Qopts[actionInd] = calculateQopt(state, actionInd, weights)
+    bestActionIndices = np.argwhere(Qopts == np.max(Qopts))
+    bestActionInd = random.choice(bestActionIndices)[0] # Make sure not always choosing first action if all valued same
+    return bestActionInd, Qopts[bestActionInd]
+
+def getFeatures(state, actionInd, shape):
+    features = np.zeros(shape)
+    features[:, actionInd] = state # TODO: Add a bias term?
+    return features
+
+def calculateQopt(state, actionInd, weights):
+    features = getFeatures(state, actionInd, weights.shape)
+    return weights.flatten().dot(features.flatten())
+
+# TODO: Add regularization?
+def updateWeights(state, actionInd, reward, nextState, weights, possibleActions):
+    features = getFeatures(state, actionInd, weights.shape)
+    [nextAction, Vopt] = getBestAction(nextState, weights, possibleActions)
+    error = calculateQopt(state, actionInd, weights) - (reward + FUTURE_DISCOUNT * Vopt)
+    return weights - STEP_SIZE * error * features
+
+
 timeStart = 60
 timeStep = 1
 year = 2015
 
 numDams = 4
 numGates = 3 # per dam
-gatesOn = np.zeros((numDams, numGates))
-gatesOn[0, 0] = 1
-gatesOn[0, 1] = 1
-gatesOn[1, 0] = 1
 
 copyInYearFiles(year, numDams)
-for i in range(30):
+possibleActions = calculatePossibleActions()
+state = getState(timeStart, year)
+weights = np.zeros((state.shape[0], possibleActions.shape[0]))
+for i in range(3):
+    actionInd = getAction(state, weights, possibleActions)
+    action = possibleActions[actionInd]
 
     for wb in range(numDams):
         wbDir = CONTROL_DIR + "wb" + str(wb + 1) + "/"
         modifyControlFile(wbDir, timeStart, timeStart + timeStep, year)
-        setAction(wbDir, timeStart, gatesOn, wb)
+        setAction(wbDir, timeStart, action, wb) # TODO: Different actions for different dams
         path = os.getcwd()
         os.chdir(wbDir)
         subprocess.check_call(['wine', CE_QUAL_W2_EXE])
@@ -85,6 +148,9 @@ for i in range(30):
         if wb != (numDams - 1):
             subprocess.check_call([CHAINING_FILE, "wb" + str(wb+1), "wb" + str(wb+2)])
 
-    outputDir = CONTROL_DIR + "wb" + str(numDams) + "/"
-    #print getReward(outputDir, gatesOn)
+    reward = getReward(numDams)
+    nextState = getState(timeStart + timeStep, year)
+    weights = updateWeights(state, actionInd, reward, nextState, weights, possibleActions)
+
     timeStart = timeStart + timeStep
+    state = nextState
