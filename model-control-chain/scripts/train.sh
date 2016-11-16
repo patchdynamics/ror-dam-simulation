cd4e4972ee31250f1bc2a7d59e4182dfd2adf68b
PatchDynamics <xone@fromthegut.org>
2016-11-16 15:35:48 -0800

    scripts/train.sh

---
+++ model-control-chain/scripts/train.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+./scripts/clear.sh
+./runSimulation.py --repeat 0 --year 2005
+./runSimulation.py --repeat 0 --year 2006
+./runSimulation.py --repeat 0 --year 2007
+./runSimulation.py --repeat 0 --year 2008
+./runSimulation.py --repeat 0 --year 2009
